c21ed4ffc5ffe166b7f19c3dcabd4e4d3e234706
Matt Turnshek <matthewturnshek@gmail.com>
2014-07-06 21:22:17 -0400

    logging

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Squome.com
 
 $( document ).ready(function() {
+
+	// Firebase setup
+	var myRootRef = new Firebase('https://popping-fire-5741.firebaseIO.com');
+
+	// Squome main logic
 	var inputBar = $("#inputBar");
 	var message = $("#message");
 
@@ -15,6 +20,10 @@ $( document ).ready(function() {
 	}
 
 	$("#input").submit(function(event) {
+		myRootRef.push({
+   			message: $("#inputBar").val(),
+   			date: (new Date()).toString()
+		});
   		event.preventDefault();
   		this.reset();
   		message.fadeIn(600, function(){});
